7f8080dec8e3e573bd9812a372d125f5a4db1916
DaisyChubb <homm0004@algonquincollege.com>
2017-12-01 19:35:26 -0500

    add documentation and more images

--- lib/CSVReader.rb
+++ lib/CSVReader.rb
@@ -1,25 +1,37 @@
+# CSVReader class
+# Reads Potato Dataset CSV file from the directory structure
+# Uses Ruby's CSV class to read in the entire dataset as a 2-dimensional array
+# Returns array for use in Potato Creator
+
+# Author: Ash-Lee Hommy 040840815
+# Nov 1st, 2017
+
 require "csv"
-
-# Read contents of file as a String
-# Parse string with ruby csv
-# create an instance of "PotatoDetail"
-# https://www.sitepoint.com/guide-ruby-csv-library-part/
-
-# Ruby stores each table row as an array
-# http://ruby-doc.org/stdlib-2.0.0/libdoc/csv/rdoc/CSV.html
 
 class CSVReader
   
-  # readFile returns a 2-dimensional array, where every row 
+  # readFile returns a 2-dimensional array of the Potato dataset, where every row 
   # from the CSV file is an element within the array
   def read_file 
-    arr_of_spuds = CSV.read("./00010014-eng.csv")
+    
+    # the Potato dataset location
+    file_location = "./00010014-eng.csv"
+    
+    # error handling
+    # throw an exception if the file can't be found
+    begin
+      puts "Attempting to read Potato dataset file"
+      
+      # return the array of Potato data
+      arr_of_spuds = CSV.read(file_location)
+      
+    rescue StandardError => e
+      # StandardError will raise to console if file is not found
+      # Specifically, a SystemCallError 
+      # Ruby Exception Tree: http://ruby.bastardsbook.com/chapters/exception-handling/#exception-tree
+      puts "Error: File not found: #{e}"
+    end
+    
   end
   
 end
-
-
-
-
-
-
--- test/models/potato_test.rb
+++ test/models/potato_test.rb
@@ -1,7 +1,40 @@
+# Potato_test class
+# Unit tests for the Potato model
+
+# Author: Ash-Lee Hommy 040840815
+# Nov 1st, 2017
 require 'test_helper'
 
 class PotatoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "creating and saving a Potato" do
+    
+    # truthy test: potato should save to the db with name and desc
+    potato = Potato.new :name => "Mr. Potato Head",
+      :desc => "A brand spankin' new potato",
+      :id => 1
+     
+    assert potato.save, "Saved the potato with name and description"
+    
+    # truthy test: locate potato by ID and ensure it is equal to the created potato
+    potato_copy = Potato.find(1)
+    
+    assert_equal potato, potato_copy
+    
+    # falsy test: potato changes details and should not the same
+    potato.name = "Mrs. Potato Head"
+    
+    !assert_equal potato, potato_copy
+    
+    # truthy test: assert that the new potato has saved
+    assert potato.save
+    
+    # truthy test: assert that a potato can be destroyed
+    assert potato.destroy
+    
+  end
+  
 end
